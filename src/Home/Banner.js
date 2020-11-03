ecb67afa90eb91940c643ddb7b6f171a5eae2995
koladeg <tayokay@yahoo.com>
2020-11-03 13:19:06 +0100

    edit home to be mobile friendly

--- src/Home/Banner.js
+++ src/Home/Banner.js
@@ -10,7 +10,7 @@ const Banner = () => {
             <div className='banner__info'>
                 <h1> Let us help you to optimize your bar experience </h1>
                 <h5> Find a bar and get a drink with ease. Let's uncover hotspots you couldn't believe. </h5>
-                <Button variant='outlined' onClick={e => history.push('explore')}>Explore Bars</Button>
+                <Button variant='outlined' onClick={e => history.push('explore') } >Explore Bars</Button>
             </div>
         </div>
     );
